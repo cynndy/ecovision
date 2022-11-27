c343b6e7a0bc9915bbc53a04906583d20934ae0b
Naman Bhatt <106633446+Namanux@users.noreply.github.com>
2022-11-28 08:50:37 +1300

    path prefixer
    nextJS config for brocken parths

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+const pathPrefix = process.env.NODE_ENV === 'production'
+  ? '/ecovision'
+  : '';
+
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: pathPrefix,
+  env: {
+    pathPrefix,
+  },
 }
+
+
+
+
+module.exports = {
+  assetPrefix: pathPrefix,
+  env: {
+    pathPrefix,
+  },
+};
