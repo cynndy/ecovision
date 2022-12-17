663802aaa0225da45abacaf941c55a20ae452c40
Naman Bhatt <106633446+Namanux@users.noreply.github.com>
2022-12-17 15:19:22 +1300

    Pre API

--- components/Layouts/Navbar.tsx
+++ components/Layouts/Navbar.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import Link from 'next/link'
 
-const Header = () => {
+const Navbar = () => {
   return (
     <div className=' bg-header flex h-12 w-full items-center justify-between border-b mt-0 p-3'>
       <span className='text-lg font-bold'><a href="/">EcoVison</a></span>
       <span><ul className='flex'>
-        <li className='p-3' ><a href="/sunglasses">Sunglasses</a></li>
-        <li className='p-3' ><a href="/glasses">Glasses</a></li>
+        {/* <li className='p-3' ><a href="/sunglasses">Sunglasses</a></li> */}
+        <li className='p-3' ><Link href={'/glasses'}>Glasses</Link></li>
         </ul></span>
       
       <span><ul className='flex'>
         <li className='p-3' ><a href="/myListing">My Listing</a></li>
-        <li className='p-3' ><a href="/signin">Sign in</a></li>
+        {/* <li className='p-3' ><a href="/signin">Sign in</a></li> */}
         </ul></span>
         
 
@@ -19,4 +19,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Navbar
