a9959563fd8633987ec57dac3e19d4ff0787f530
kriziatenorio <114276794+kriziatenorio@users.noreply.github.com>
2022-12-22 13:37:57 +0000

    fixed daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,24 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    // './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    // extend: {},
-    colors: {
-      'primary': '#6A983C',
-      'secondary': '#84B94E',
-      'white': '#ffffff',
-      'light': '#F4F8EC',
-      'red': '#BE123C',
-      'gray-100': '#F2F4F7',
-      'gray-300': '#D0D5DD',
-      'gray-500': '#667085',
-      'gray-700': '#344054'
-    }
+    extend: {
+      colors: {
+        'primary': '#6A983C',
+        'secondary': '#84B94E',
+        'white': '#ffffff',
+        'light': '#F4F8EC',
+        'red': '#BE123C',
+        'gray-100': '#F2F4F7',
+        'gray-300': '#D0D5DD',
+        'gray-500': '#667085',
+        'gray-700': '#344054'
+      }
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')]
 }
 
 
