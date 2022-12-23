1ba21e467e5fa849075c0322082cdcfb26c43a48
Krizia Tenorio <kriziatenorio@gmail.com>
2022-12-23 19:09:51 +0800

    fixed mappings

--- components/CardListing.tsx
+++ components/CardListing.tsx
@@ -2,19 +2,19 @@ import Image from 'next/image'
 
 const CardListing = (props : any) => {
     return (
-        <div className="pl-8 pb-8 card card-side bg-base-100" key={props.data.id}>
+        <div className="pl-8 pb-8 card card-side bg-base-100" key={props.id}>
             <figure>
-            <Image src={props.data.photo} alt="image" width={250} height={250} />
+                <Image src={props.photo} alt="image" width={250} height={250} />
             </figure>
             <div className="card-body border">
-            <h2 className="card-title">Black Sunglasses</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h2 className="card-title">{props.title}</h2>
+            <p>{props.description}</p>
             <h3 className="font-bold">$70</h3>
 
             <div className="card-actions justify-end">
                 <div className='flex space-x-4'>
-                <button className='h-14 cursor-pointer rounded-xl bg-[#6A983C] px-8 font-semibold text-black hover:text-white hover:shadow-xl'>Edit</button> 
-                <button className='h-14 cursor-pointer rounded-xl border border-[#6A983C] px-8 font-semibold text-black hover:text-[#6A983C] hover:shadow-xl'>Delete </button>
+                    <button className='h-14 cursor-pointer rounded-xl bg-[#6A983C] px-8 font-semibold text-black hover:text-white hover:shadow-xl'>Edit</button> 
+                    <button className='h-14 cursor-pointer rounded-xl border border-[#6A983C] px-8 font-semibold text-black hover:text-[#6A983C] hover:shadow-xl'>Delete </button>
                 </div>
             </div>
             
